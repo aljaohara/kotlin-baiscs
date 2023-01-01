c792731401b9a08048411d33136e47e3c0ccd76b
aljohara alfassam <111182521+aljaohara@users.noreply.github.com>
2023-01-01 18:55:51 +0300

    Add files via upload

---
+++ aljoharafirstassignment.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+fun main()
+{
+   var a = "abdullah"
+    var b : Float = 20.6f
+    var c : Double = 8.9746325
+    var d : Byte = 25
+    var e : Int = 2022
+    var f : Long = 321984526954
+    var g : Boolean = true
+    var h : Char ='a'
+println(a)
+ println(b)
+ println(c)
+ println(d)
+ println(e)
+ println(f)
+ println(g)
+ println(h)
+
+
+
+
+
+
+
+
+}
